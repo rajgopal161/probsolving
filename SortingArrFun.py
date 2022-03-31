2ca1a63fdeb8da0e785c3483e678c329991e74b4
rajgopal161 <97560099+rajgopal161@users.noreply.github.com>
2022-03-31 12:34:14 -0600

    Update SortingArrFun.py

--- SortingArrFun.py
+++ SortingArrFun.py
@@ -7,15 +7,14 @@ def sortarr(arr):
             if arr[n] > arr[n+1]:
                 temp = arr[n]
                 arr[n] = arr[n+1]
-                arr[n+1] = temp
-                
-            
+                arr[n+1] = temp      
             n += 1
-        if count == (arrlen-3):
-            return(arr)
+            
         n = 0
         count += 1
         
+        if count == (arrlen-3):
+            return(arr)        
         
     
 arr = [6,3,7,9,4,8,1,2,5]
