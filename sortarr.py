c5d1508cf0807351e0f2b3bf9df8ce2b15051436
rajgopal161 <97560099+rajgopal161@users.noreply.github.com>
2022-03-31 06:49:41 -0600

    Create sortarr.py

---
+++ sortarr.py
@@ -0,0 +1,16 @@
+arr = [6,3,7,9,4,1,2,5]
+arrlen = len(arr)
+count = 0
+n = 0
+
+while count <= arrlen:
+    while n <= (arrlen-2):
+        if arr[n] > arr[n+1]:
+            temp = arr[n]
+            arr[n] = arr[n+1]
+            arr[n+1] = temp
+            print(arr)
+        n += 1
+        count += 1
+    n = 0
+    count = 0
