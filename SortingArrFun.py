5659ca575361dab892c068beee2475d48de94aa7
rajgopal161 <97560099+rajgopal161@users.noreply.github.com>
2022-03-31 12:30:39 -0600

    Create SortingArrFun.py

---
+++ SortingArrFun.py
@@ -0,0 +1,22 @@
+def sortarr(arr):
+    arrlen = len(arr)
+    count = 0
+    n = 0
+    while count <= arrlen-3:
+        while n <= (arrlen-2):
+            if arr[n] > arr[n+1]:
+                temp = arr[n]
+                arr[n] = arr[n+1]
+                arr[n+1] = temp
+                
+            
+            n += 1
+        if count == (arrlen-3):
+            return(arr)
+        n = 0
+        count += 1
+        
+        
+    
+arr = [6,3,7,9,4,8,1,2,5]
+print(sortarr(arr))
